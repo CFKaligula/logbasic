9496162f663cc1cd4a4ade83da5d7d8d2d927ac2
Casper Kole <casper@voltgoed.nl>
2024-10-07 15:02:44 +0200

    0.0.1

--- src/logbasic/__init__.py
+++ src/logbasic/__init__.py
@@ -19,7 +19,7 @@ LOG_DEBUGGING_ENV_VAR: Final = 'log_debugging'
 
 
 def debug(*args: list) -> None:
-    debugging = os.environ[LOG_DEBUGGING_ENV_VAR] or DEBUGGING
+    debugging = bool(os.environ.get(LOG_DEBUGGING_ENV_VAR, 'false')) or DEBUGGING
     if debugging:
         format_and_print(ColorCode.grey, LogTypeText.debug, *args)
 
